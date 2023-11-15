2e16fbd8c654e26ec5d197515a048353d6c64cf4
hongjyoun <hongjyoun@gmail.com>
2023-11-15 17:43:59 +0900

    MainTitle 위젯 분리

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,5 +1,4 @@
-import 'package:app_two/constants/gaps.dart';
-import 'package:app_two/constants/sizes.dart';
+import 'package:app_two/widgets/main_title.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -12,44 +11,7 @@ class MyHomePage extends StatelessWidget {
       body: SafeArea(
         child: FractionallySizedBox(
           widthFactor: 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/logo.png'),
-                  ),
-                  Gaps.h12,
-                  Column(
-                    children: [
-                      Gaps.v10,
-                      Text(
-                        "이리오냥",
-                        style: TextStyle(
-                          color: Color(0xFFE37D7D),
-                          fontSize: Sizes.size40,
-                          fontFamily: 'SeoulHangangEB',
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Gaps.v10,
-              Text(
-                "고양이로 고양이를 불러보자",
-                style: TextStyle(
-                  color: Color(0xFFA7B9B0),
-                  fontSize: Sizes.size20,
-                  fontFamily: 'SeoulHangangB',
-                  letterSpacing: 0.2,
-                ),
-              ),
-            ],
-          ),
+          child: MainTitle(),
         ),
       ),
     );
---
+++ lib/widgets/main_title.dart
@@ -0,0 +1,49 @@
+import 'package:app_two/constants/gaps.dart';
+import 'package:app_two/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class MainTitle extends StatelessWidget {
+  const MainTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/images/logo.png'),
+            ),
+            Gaps.h12,
+            Column(
+              children: [
+                Gaps.v10,
+                Text(
+                  "이리오냥",
+                  style: TextStyle(
+                    color: Color(0xFFE37D7D),
+                    fontSize: Sizes.size40,
+                    fontFamily: 'SeoulHangangEB',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Gaps.v10,
+        Text(
+          "고양이로 고양이를 불러보자",
+          style: TextStyle(
+            color: Color(0xFFA7B9B0),
+            fontSize: Sizes.size20,
+            fontFamily: 'SeoulHangangB',
+            letterSpacing: 0.2,
+          ),
+        ),
+      ],
+    );
+  }
+}
