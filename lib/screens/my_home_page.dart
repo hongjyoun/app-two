105527b67373501e143c35797eaec2cd1a239b25
hongjyoun <hongjyoun@gmail.com>
2023-11-15 17:32:47 +0900

    constant 추가 및 대문 타이틀 추가

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_two/constants/gaps.dart';
+import 'package:app_two/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -5,6 +7,51 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Color(0xFFF0E2C6),
+      body: SafeArea(
+        child: FractionallySizedBox(
+          widthFactor: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/logo.png'),
+                  ),
+                  Gaps.h12,
+                  Column(
+                    children: [
+                      Gaps.v10,
+                      Text(
+                        "이리오냥",
+                        style: TextStyle(
+                          color: Color(0xFFE37D7D),
+                          fontSize: Sizes.size40,
+                          fontFamily: 'SeoulHangangEB',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Gaps.v10,
+              Text(
+                "고양이로 고양이를 불러보자",
+                style: TextStyle(
+                  color: Color(0xFFA7B9B0),
+                  fontSize: Sizes.size20,
+                  fontFamily: 'SeoulHangangB',
+                  letterSpacing: 0.2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
