eb1fe69833215c1066eba73be7a70f943441eefb
hongjyoun <hongjyoun@gmail.com>
2023-11-20 08:48:49 +0900

    cat & background & sound package installed

--- lib/screens/cat_sound_screen.dart
+++ lib/screens/cat_sound_screen.dart
@@ -1,10 +1,53 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-class CatSoundScreen extends StatelessWidget {
+class CatSoundScreen extends StatefulWidget {
   const CatSoundScreen({super.key});
 
   @override
+  State<CatSoundScreen> createState() => _CatSoundScreenState();
+}
+
+class _CatSoundScreenState extends State<CatSoundScreen> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  void _playSound() {
+    // 소리 파일 경로
+    // String soundPath = 'assets/sounds/my_sound.mp3';
+
+    // 소리 파일을 재생
+    _audioPlayer.play(UrlSource('https://example.com/my-audio.wav'));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).canvasColor,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                const Text("slkdfj"),
+                GestureDetector(
+                  onTap: _playSound,
+                  child: const Image(
+                    image: AssetImage('assets/images/cat1.png'),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
