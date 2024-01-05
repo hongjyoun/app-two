06b2e32d210f66d4a905f829de09bb31c96e78fd
hongjyoun <hongjyoun@gmail.com>
2024-01-05 14:47:51 +0900

    firebase added & Google analytics info plist added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:app_two/firebase_options.dart';
 import 'package:app_two/screens/my_home_page.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AppTwo());
 }
 
 class AppTwo extends StatelessWidget {
   const AppTwo({super.key});
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
 
   @override
   Widget build(BuildContext context) {
